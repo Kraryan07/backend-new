56ccb6860321abed57c9fcb3662b7a54a092c408
Kumar Aryan <kraryan12050@gmail.com>
2025-07-16 02:41:38 +0530

    middlewares cloudinary

--- src/api.js
+++ src/api.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
+//routes import
+import userRouter from './routes/user.routes.js'
+ 
+//route declaration http:\\localhos t:
+app.use("/api/v1/users",userRouter)
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import dotenv from 'dotenv';
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constraints";
-
+import { app } from './api.js';
 import connectDb from "./db/index.js";
 
 dotenv.config({
   path:'./env'
 })
 
-connectDb()
-.then(()=>{
+connectDb().then(()=>{
   app.listen(process.env.PORT||8000,()=>{
     console.log(`server is running on port ${process.env.PORT}`)
   })
